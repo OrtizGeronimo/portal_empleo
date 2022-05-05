bad268d3a5ac8828e58e97955fbfe45e1cef5ffd
Gero <geroortizz123@gmail.com>
2022-05-05 16:16:13 -0300

    primer commit

---
+++ src/main/java/com/example/portal_empleo/PortalEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.example.portal_empleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalEmpleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalEmpleoApplication.class, args);
+    }
+
+}
