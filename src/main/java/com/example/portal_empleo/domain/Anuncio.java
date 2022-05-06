42383f9cc12f8f9e4274804159a988c9d5ecd793
Nicolas <nicosaurina7@gmail.com>
2022-05-06 02:27:07 -0300

    Añadí anuncio

--- src/main/java/com/example/portal_empleo/domain/Anuncio.java
+++ src/main/java/com/example/portal_empleo/domain/Anuncio.java
@@ -1,22 +1,26 @@
 package com.example.portal_empleo.domain;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.List;
+
 
 @Entity
 @Data
-@Table(name = "empresas")
+@Table(name = "anuncios")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Empresa {
+
+public class Anuncio {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "fk_empresa")
+    private Empresa empresa;
 
 }
---
+++ src/main/java/com/example/portal_empleo/repositories/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package com.example.portal_empleo.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository {
+}
