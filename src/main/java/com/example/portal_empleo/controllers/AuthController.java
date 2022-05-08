9511bfd50eca57d4d7d09bc20049c76576018173
Gero <geroortizz123@gmail.com>
2022-05-08 18:09:23 -0300

    seguridad y empezamos con las vistas

--- src/main/java/com/example/portal_empleo/controllers/AuthController.java
+++ src/main/java/com/example/portal_empleo/controllers/AuthController.java
@@ -1,35 +1,37 @@
 package com.example.portal_empleo.controllers;
 
-import com.example.portal_empleo.config.User;
 import com.example.portal_empleo.domain.Usuario;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
+@RequestMapping("")
 public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPrincipal(){
+        return "login";
+    }
+
+
 
     @PostMapping("/login")
     public String login(@RequestParam("user") String username, @RequestParam("psw") String psw) {
         try {
             Usuario usuario = null; //encontrar el usuario, y según el tipo del mismo vamos a darle los permisos
             if (usuario == null) {
-                return "";
+                return "views/login";
             }
             String token = getJWTToken(username);
-            User u = new User();
-            u.setUsr(username);
-            u.setToken(token);
-            return "";
+            return "views/login";
         } catch (Exception e) {
             return "";
         }
