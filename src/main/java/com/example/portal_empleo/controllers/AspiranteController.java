e559b31dc8bd8626995ce1c57b9c33b80ed5a0d2
Gero <geroortizz123@gmail.com>
2022-05-25 16:57:19 -0300

    url

--- src/main/java/com/example/portal_empleo/controllers/AspiranteController.java
+++ src/main/java/com/example/portal_empleo/controllers/AspiranteController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "")
+@RequestMapping(path = "/candidate")
 public class AspiranteController {
 }
